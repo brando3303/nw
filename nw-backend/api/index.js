36c3fd842b4bf1c7df5a268e5c8ef597531fe6ae
brandon <brando3303@gmail.com>
2025-04-06 21:34:09 -0700

    fixes

--- nw-backend/api/index.js
+++ nw-backend/api/index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const postgres = require("postgres");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const cors = require("cors")
 
 const port = 3001;
 const app = express();
+
+app.use(cors({
+    origin: '*' // or use '*' to allow all origins
+}));
+  
 app.use(bodyParser.json());
 app.get('/playerList', routes.getPlayerList);
 app.get('/player', routes.getPlayer);
