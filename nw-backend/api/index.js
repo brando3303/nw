772e3e8a60246068a935b305bb690431c6502af1
brandon <brando3303@gmail.com>
2025-04-24 08:35:33 -0700

    added mobile css styling. mixed styles, should be unified eventually.

--- nw-backend/api/index.js
+++ nw-backend/api/index.js
@@ -9,18 +9,15 @@ const cors = require('cors');
 const allowedOrigins = ['https://nsf-nateleland.vercel.app', 'http://localhost:3000', 'https://www.nsf-nateleland.com'];
 const corsOptions = {
     origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) { //also allow requests made directly to the server
-        callback(null, true);
-    } else {
-        callback(new Error('Not allowed by CORS'));
-    }
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) { //also allow requests made directly to the server
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
     },
 };
 app.use(cors(corsOptions));
     
-
-
-
 app.use(bodyParser.json());
 app.get('/playerRoster', routes.getPlayerList);
 app.get('/player', routes.getPlayer);
--- nw-frontend/src/PageBorders.js
+++ nw-frontend/src/PageBorders.js
@@ -6,48 +6,58 @@ import { isMobile } from "react-device-detect";
 
 
 export class TitleBar extends Component {
-
-  renderMobileUpdate = () => {
+  
+  renderMobile = (element) => {
     if (!isMobile) {
       return;
     }
-    return <p>Mobile UI updates coming soon!</p>
+    return element;
   }
 
+  renderNotMobile = (element) => {
+    if (isMobile) {
+      return;
+    }
+    return element;
+  }
+  
   render = () => {
     return (
       <div>
-        <div className={styles.navbar}>
-          <img src={NSFlogo} alt={"Logo"}/>
-          <Link to={"/home"} className={styles.navbar_item_back}>
-            <span className={styles.navbar_item}>Home</span>
-          </Link>
-          <Link to={"/home"} className={styles.navbar_item_back}>
-            <span className={styles.navbar_item}>About</span>
-          </Link>
-          <p className={styles.subtext}>College Football scouting reports by Nate Leland</p>
-        </div>
-        {this.renderMobileUpdate()}
+      <div className={styles.navbar}>
+      <img src={NSFlogo} alt={"Logo"}/>
+      <Link to={"/home"} className={styles.navbar_item_back}>
+      <span className={styles.navbar_item}>Home</span>
+      </Link>
+      <Link to={"/home"} className={styles.navbar_item_back}>
+      <span className={styles.navbar_item}>About</span>
+      </Link>
+      {this.renderNotMobile(<p className={styles.subtext}>College Football scouting reports by Nate Leland</p>)}
       </div>
-    )};
-  }
-
-
+      {this.renderMobile(<p className={styles.mobile_subtext}>College Football scouting reports by Nate Leland</p>)}
+      {this.renderMobile(<p className={styles.mobile_subtext}>Mobile Updates Coming Soon!</p>)}
+      </div>
+    )
+  };
+}
+  
+  
   
   
   export class Footer extends Component {
     render = () => {
       return (<footer className={styles.footer}>
         <div className={styles.footer_content}>
-          <div className={styles.footer_section}>
-            <a href="https://www.linkedin.com/in/nate-leland-3b255b346/" target="_blank" rel="noopener noreferrer">
-              LinkedIn/NateLeland
-            </a>
-          </div>
+        <div className={styles.footer_section}>
+        <a href="https://www.linkedin.com/in/nate-leland-3b255b346/" target="_blank" rel="noopener noreferrer">
+        LinkedIn/NateLeland
+        </a>
+        </div>
         <div className={styles.footer_section}>
         <p>Contact: <a href="nfleland@gmail.com">nfleland@gmail.com</a></p>
         </div>
         </div>
-        </footer>);
-      }
+        </footer>
+      );
     }
+  }
