e04df67da7358a1f8fc627cc01d4e4f8b4d30599
brandon <brando3303@gmail.com>
2025-04-30 14:34:09 -0700

    push search features to server

--- nw-backend/api/routes.js
+++ nw-backend/api/routes.js
@@ -17,6 +17,44 @@ const getPlayer = async (req, res) => {
     return;
 }
 
+const searchPages = async (req, res) => {
+    const sql = await getDB();
+    const search = req.query.search;
+    if (search == null) {
+        res.status(400).send("invalid search query");
+        return;
+    }
+    // step 1: get word index
+    const results = await sql`SELECT * FROM playerpage_word_index WHERE word LIKE ${search + '%'}`;
+    if (results.length == 0) {  // no word like this found in db
+        res.status(200).send(JSON.stringify([]));
+        return;
+    }
+    const wordIndex = results[0].index; // get the word index for the first result
+    const closestWord = results[0].word; // get the closest word for the first result
+
+    // step 2: get the  player pages indexes that contain this word
+    const playerPageIndices = await sql`SELECT * FROM playerpage_inverted_index WHERE word_index = ${wordIndex}`;
+    if (playerPageIndices.length == 0) {  // no player pages with this word found in db
+        res.status(200).send(JSON.stringify([]));
+        return;
+    }
+
+    // step 3: get all players with these indices
+    const playerIds = playerPageIndices.map(p => p.player_name_index);
+
+    const players = await sql`SELECT * FROM playerpage_name_index WHERE index = ANY(${playerIds})`;
+    if (players.length == 0) {  // no players with this word found in db
+        res.status(200).send(JSON.stringify([]));
+        return;
+    }
+
+    const ret = {players: players, closestWord: closestWord};
+    res.status(200).send(JSON.stringify(ret));
+    return;
+}
+
+
 const getDB = async () => {
     return postgres(connectionString);
 }
@@ -32,4 +70,5 @@ const getPlayerFromDB = async (sql, id) => {
 module.exports = {
     getPlayerList,
     getPlayer,
+    searchPages,
 }
